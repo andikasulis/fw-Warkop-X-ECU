935d024526992d6694a7be2281df9aa5b53e279f
Andika Sulis <andikasulispratama@yahoo.co.id>
2025-12-21 18:24:55 +0700

    feat: Set pin to universal board

--- generated/controllers/generated/signature_warkop_x.h
+++ generated/controllers/generated/signature_warkop_x.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 1258977499
-#define TS_SIGNATURE "rusEFI master.2025.12.21.warkop_x.1258977499"
+#define SIGNATURE_HASH 1045950243
+#define TS_SIGNATURE "rusEFI master.2025.12.21.warkop_x.1045950243"
